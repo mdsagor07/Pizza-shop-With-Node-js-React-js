da7979d4d4a12c0281b5a22689fbc4090f1be76d
mdsagor07 <sagor.cse.just@gmail.com>
2021-08-20 16:32:10 +0600

    Navbar

---
+++ client/src/screens/Homescreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import pizzas from'../pizzasdata'
+export default function Homescreen() {
+    return (
+        <div>
+            <h1>{pizzas.length}</h1>
+        </div>
+    )
+}
